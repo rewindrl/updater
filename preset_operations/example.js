f34c24a4958b7fa2df61c0b558db89aff041ec1c
Barnaby Collins <barnabyc@sky.com>
2020-09-02 18:54:15 +0100

    Direct people to the master branch

--- preset_operations/example.js
+++
@@ -1,8 +0,0 @@
-// Example custom operation for Rewind Gaming's broadcast updater
-// Author: Barnaby 'bucketman' Collins
-
-const exampleOperation = {
-    'name': 'example',
-    'operation': (id, _) => document.getElementById(id).innerHTML = 'big bucket',
-    'isSimple': true
-};
